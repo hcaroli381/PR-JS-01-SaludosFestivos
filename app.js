6f653266a8c028c81ed2e30dea9bdc4f19b03711
hcaroli381 <hcaroli381@g.educaand.es>
2026-01-13 14:19:52 +0100

    Practica 1 completada

---
+++ app.js
@@ -0,0 +1,18 @@
+const formulario1 = document.querySelector("#formulario");
+const resultado = document.querySelector("#resultado");
+let emojis = "";
+formulario1.addEventListener("click", function (event) {
+    event.preventDefault();
+    const name1 = document.querySelector("#name").value; 
+    const age = document.querySelector("#age").value;
+    if (age < 18) {
+        for (let i = 0; i < age; i++){
+            emojis = emojis + "🥛";
+        }
+    } else {
+        for (let i = 0; i < age; i++){
+            emojis = emojis + "🍺";
+        }
+    }
+    resultado.innerHTML = `Hola ${name1}!<br>Brindemos con ${emojis}`;
+});
